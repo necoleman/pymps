6527e8a1075b16ffc87a6d2a4c57ac9ce10fe6ad
necoleman <coleman.neal@gmail.com>
2016-08-17 21:48:06 -0500

    Got it (mostly) working!

--- Ldrum.py
+++ Ldrum.py
@@ -10,10 +10,17 @@ import scipy.linalg as linalg
 N = 36
 num_pts = 2*N
 k = np.arange(1,N).reshape(N-1,1)
-t1 = 1.5*np.pi*np.arange(0.5,num_pts-0.5)/num_pts
+t1 = 1.5*np.pi*np.linspace(0.5,num_pts-0.5,num=num_pts)/num_pts
 r1 = 1./np.maximum(np.abs(np.sin(t1)), np.abs(np.cos(t1)))
+
+#print len(t1)
+#print len(r1)
+
 t2 = 1.5*np.pi*np.random.random(num_pts)
 r2 = np.random.random(num_pts)/np.maximum(np.abs(np.sin(t2)),np.abs(np.cos(t2)))
+
+#print len(t2)
+#print len(r2)
 
 #print k
 #print t1
@@ -28,19 +35,43 @@ t = np.concatenate( (t1,t2) )
 r = np.concatenate( (r1,r2) )
 lamvec = np.arange(0.2,25,0.2)
 
+for n in range(len(t)):
+	print r[n]*np.cos(t[n]), r[n]*np.sin(t[n])
+
 #print t
 #print r
 #print lamvec
 
 S = []
+
+#print t.shape
+#print r.shape
+#print lamvec.shape
+
 for lam in lamvec:
 	A = np.sin(2*t*k/3.)*sp.jv(2*k/3, np.sqrt(lam)*r)
-	[Q,R] = linalg.qr(A)
-	print A.shape
-	print Q.shape
-	print R.shape
-	print '*'*5
-	s = np.min(linalg.svd(Q[:num_pts,:])[1])
+	A = A.T
+	[Q,R] = linalg.qr(A,mode='economic')
+	#print A.shape
+	#print Q.shape
+	#print R.shape
+	#print Q[:num_pts,:].shape
+	#print '*'*5
+	s = np.min(linalg.svd(Q[:num_pts-1,:])[1])
+	#print s
 	S.append(s)
 
-#print S
+S = np.array(S)
+
+#for t in range(len(S)):
+#	print lamvec[t], S[t]
+
+
+
+T = range(len(S))
+minima = []
+for t in T[1:-1]:
+	if S[t] < S[t+1] and S[t] < S[t-1]:
+		minima.append(lamvec[t])
+
+#print minima
--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 Adapted from Betcke-Trevethen, 2003, *Reviving the Method of Particular 
 Solutions.*
 
-Currently running into a bug where all singular values of the slice of Q 
-I'm interested in are 1. This is probably because that slice is still 
-orthogonal.
+Implementation of code from Betcke-Trevethen 2003 is 95% complete. Still
+an issue with the magnitude of the eigenvalues --- suspect problem is
+in the scaling of the domain. To plot the points in the domain, pipe
+the output of Ldrum.py to a file and then plot with gnuplot. To plot
+the lowest singular value, comment the printing of r and t, uncomment
+the printing of lamvec and S, pipe to a file, and plot with gnuplot.
